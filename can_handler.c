88233b4256a8e45a25c7ce28a7d0af4907d433b6
ping77 <c.diouf.13@groupe-esigelec.fr>
2016-01-30 15:20:54 +0100

    Update can_handler.c

--- can_handler.c
+++ can_handler.c
@@ -11,17 +11,26 @@
  * Include header files for all drivers that have been imported from
  * Atmel Software Framework (ASF).
  */
-#include <can.h>
+//#include <can.h>
 #include <stdint.h>
 #include "can_isotp.h"
-#include <pmc.h>
+//#include <pmc.h>
+#include <asf.h>
 #include "arduino_can.h"
+#include "reception_can.h"
+#include "envoi_can.h"
+
+/*	VAriables globales	*/
 IsoTpMessage msg;	
+int message[4095];
+uint16_t *last_length; 
+volatile uint32_t g_ul_recv_status;
+can_mb_conf_t can_mb0, can_mb1;
+
 /*	
 **	CAN initializer
 **
 */
-volatile uint32_t g_ul_recv_status;
 void can_initialiser(
 can_mb_conf_t can0_mailbox0,
 can_mb_conf_t can0_mailbox1
@@ -31,7 +40,7 @@ can_mb_conf_t can0_mailbox1
 	//	Activation de l'horloge pour le contrôleur CAN0
 	pmc_enable_periph_clk(ID_CAN0);
 	//	Fixation d'une vitesse par défaut du bus: 500Kb/s
-	can_init(CAN0, MCK_FOR_CAN_21, CAN_BPS_500K);
+	//can_init(CAN0, MCK_FOR_CAN_21, CAN_BPS_500K);
 	//	Activer le contrôleur can0
 	can_enable(CAN0);
 	//	Initialisation des structures de transfert
@@ -71,6 +80,45 @@ can_mb_conf_t can0_mailbox1
 
 }
 
+static bool my_flag_autorize_cdc_transfert = false;
+bool my_callback_cdc_enable(void)
+{
+	my_flag_autorize_cdc_transfert = true;
+	return true;
+}
+void my_callback_cdc_disable(void)
+{
+	my_flag_autorize_cdc_transfert = false;
+}
+void task(void)
+{
+	if (my_flag_autorize_cdc_transfert) {
+		udi_cdc_putc('A');
+		udi_cdc_getc();
+		// Waits and gets a value on CDC line
+		//int udi_cdc_getc(void);
+		// Reads a RAM buffer on CDC line
+		//iram_size_t udi_cdc_read_buf(int* buf, iram_size_t size);
+		// Puts a byte on CDC line
+		//int udi_cdc_putc(int value);
+		// Writes a RAM buffer on CDC line
+		//iram_size_t udi_cdc_write_buf(const int* buf, iram_size_t size);
+		//bool var=udi_cdc_is_rx_ready(); // verifier si un caractère est reçu
+
+	}
+}
+
+void usb_init(
+void
+)
+{
+	// Authorize interrupts 
+	irq_initialize_vectors();
+	cpu_irq_enable();
+	// Initialize the sleep manager service 
+	sleepmgr_init();
+	udc_start();
+}
 /*
 **	Choice handler
 **	Fonction d'interactivité avec l'utilisateur	
@@ -80,23 +128,28 @@ void choice_handler(
 uint16_t our_id
 )
 {
+	uint32_t baudrate=250;	//	Par défaut mais il prendra la valeur communiquée par le Host
 	switch(our_id)
 	{
 		case SEND_MESSAGE:
 		{
-			
+			receive_message(
+			msg,
+			message,
+			last_length
+			);
 		} break;
-		case SET_MANUAL_BAUDRATE:
-		{
-			
-		}break;
 		case SET_AUTO_BAUDRATE:
 		{
-			
+			can_set_baudrate(
+			CAN0,
+			MCK_FOR_CAN_42,
+			baudrate
+			);
 		}break;
 		case LISTEN_ONLY:
 		{
-			
+			can_enable_autobaud_listen_mode(CAN0);
 		}break;
 	}
 }
